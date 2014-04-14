d476372a76ff7432f5c27efefb3440c737db5f48
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-04-14 08:31:02 -0700

    file copy by reading char by char until it reaches EOF

---
+++ file-copy-3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int c;
+    FILE *in, *out;
+    
+    in = fopen("file.in","r");
+    out = fopen("file.out","w");
+    
+    while((c=getc(in)) != EOF)
+        fputc(c,out);
+    exit(0);
+}
